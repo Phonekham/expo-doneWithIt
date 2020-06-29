59ce67290b5e421adab77bac7bf6ec85f69edb43
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-06-29 09:59:13 +0700

    access the library

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import React,{useEffect} from "react";
-import * as ImagePicker from 'expo-image-picker'
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
-const requestPermission=async()=>{
-  const result=await ImagePicker.requestCameraRollPermissionsAsync()
-  if (!result.granted) {
-    alert('U need to enable permission')
-  }
-}
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!result.granted) {
+      alert("U need to enable permission");
+    }
+  };
 
   useEffect(() => {
-  requestPermission()
-  }, [])
-  return <Screen></Screen>
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (err) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage}></Button>
+      <Image
+        source={{ uri: imageUri }}
+        style={{ width: 200, height: 200 }}
+      ></Image>
+    </Screen>
+  );
 }
