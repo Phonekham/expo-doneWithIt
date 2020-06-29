90b02974ba93a888e2d6569d9028a5ef85c008f3
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-06-29 11:36:05 +0700

    image input list scrollview

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import ImageInput from "./ImageInput";
+import { ScrollView } from "react-native-gesture-handler";
 
 const ImageInputList = ({ imageUris = [], onRemoveImage, onAddImage }) => {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput
-            imageUri={uri}
-            key={uri}
-            onChangeImage={() => onRemoveImage(uri)}
-          ></ImageInput>
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                key={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              ></ImageInput>
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)}></ImageInput>
         </View>
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)}></ImageInput>
+      </ScrollView>
     </View>
   );
 };
