c820ca5b566934c6ed19481ec1cc25f563d3a31e
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-06-30 13:34:30 +0700

    FINISH NAVIGATION

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,18 +1,61 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import ListingsScreen from "../screens/ListingsScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import AccountScreen from "../screens/AccountScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
+import NewListingButton from "./NewListingButton";
 
 const Tab = createBottomTabNavigator();
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator}></Tab.Screen>
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen}></Tab.Screen>
-    <Tab.Screen name="Account" component={AccountNavigator}></Tab.Screen>
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            name="home"
+            size={size}
+            color={color}
+          ></MaterialCommunityIcons>
+        ),
+      }}
+    ></Tab.Screen>
+    <Tab.Screen
+      name="ListingEdit"
+      component={ListingEditScreen}
+      options={({ navigation }) => ({
+        tabBarButton: () => (
+          <NewListingButton
+            onPress={() => navigation.navigate("ListingEdit")}
+          ></NewListingButton>
+        ),
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            name="plus-circle"
+            size={size}
+            color={color}
+          ></MaterialCommunityIcons>
+        ),
+      })}
+    ></Tab.Screen>
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            name="account"
+            size={size}
+            color={color}
+          ></MaterialCommunityIcons>
+        ),
+      }}
+    ></Tab.Screen>
   </Tab.Navigator>
 );
 
