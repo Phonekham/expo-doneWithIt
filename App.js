e24b2000292f52de1d5348902c3fc132257535f0
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-06-30 09:57:41 +0700

    stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+import { Text } from "react-native";
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets}></Stack.Screen>
+    <Stack.Screen name="TweetsDetails" component={TweetDetails}></Stack.Screen>
+  </Stack.Navigator>
+);
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweets details</Text>
+  </Screen>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator></StackNavigator>
+    </NavigationContainer>
+  );
 }
