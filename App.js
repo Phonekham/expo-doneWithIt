934f4f3edb9594212cd092a66daa5cc1617fa8f1
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-07-08 09:11:21 +0700

    Refactoring

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
-import jwtDecode from "jwt-decode";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/hooks/context";
 import authStorage from "./app/auth/storage";
@@ -13,16 +12,15 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const storeToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
 
   if (isReady)
     return (
       <AppLoading
-        startAsync={storeToken}
+        startAsync={restoreUser}
         onFinish={() => setIsReady(false)}
       ></AppLoading>
     );
